bf12a12a6db1cabd3665f13e32dbd59df016d621
Chisom Laura <102640478+ChisomLaura@users.noreply.github.com>
2022-06-15 19:17:38 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var available = document.querySelector("h3");
+console.log(available);
+// <h3> We're here for you every day of the week.</h3>
+
+var mainTitle = document.querySelector(".main-title");
+console.log(mainTitle);
+// <h1 class="main-title"> Ryan's Roses</h1>
+
+var firstItem = document.querySelector("ul li");
+console.log(firstItem);
+// <li> Today's Specials </li>
+
+var intro = document.querySelector(".intro p");
+console.log(intro);
+// <p> Available today </p>
